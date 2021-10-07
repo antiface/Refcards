5f02f7b46f3e3b3cdbed165f3171fcdce16d7f94
A.G <gagnon.alex@gmail.com>
2021-10-06 22:22:17 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 drop table if exists refcards;
     create table refcards (
         id integer primary key autoincrement,
+        created_at DATETIME DEFAULT (STRFTIME('%d-%m-%Y   %H:%M:%S', 'NOW','localtime')),
         context text not null,
         refcard text not null
 );
